4973ca1a6c816fc4f3bd3170df21cde829ebb2f2
Ckck12 <82868442+Ckck12@users.noreply.github.com>
2022-07-26 00:34:04 +0900

    React basic import react,

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <>
+    <h1>hello world</h1>
+    <p>hello react</p>
+  </>,
+  document.getElementById("root")
+);
